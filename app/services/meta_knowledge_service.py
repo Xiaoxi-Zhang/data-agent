0dc5744c94ed4762d070e98497c84532eb7a5804
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2026-09-15 23:56:11 +0800

    根据meta_config.yaml配置文件构建meta_config.py脚本，读取元数据库服务配置文件

--- app/services/meta_knowledge_service.py
+++ app/services/meta_knowledge_service.py
@@ -1,9 +1,24 @@
 from pathlib import Path
 
+from omegaconf import OmegaConf
+
+from app.conf.meta_config import MetaConfig
+
 
 class MetaKnowledgeService:
     def __init__(self):
         pass
 
     async def build(self, config_path: Path):
-        pass
+        # 1.读取配置文件
+        context = OmegaConf.load(config_path)
+        schema = OmegaConf.structured(MetaConfig)
+        meta_config: MetaConfig = OmegaConf.to_object(OmegaConf.merge(schema, context))
+        print(meta_config.metrics)
+
+        # 2.根据配置文件同步指定的表信息和指标信息
+        if meta_config.tables:
+            pass
+
+        if meta_config.metrics:
+            pass
