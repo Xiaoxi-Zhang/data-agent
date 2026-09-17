2ebea7fed88085a8cce810938750e4071405adcc
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2026-09-17 12:23:38 +0800

    根据配置信息保存表信息和字段信息到数据库

--- app/services/meta_knowledge_service.py
+++ app/services/meta_knowledge_service.py
@@ -3,11 +3,17 @@ from pathlib import Path
 from omegaconf import OmegaConf
 
 from app.conf.meta_config import MetaConfig
+from app.models.column_info import ColumnInfoMySQL
+from app.models.table_info import TableInfoMySQL
+from app.repositories.mysql.dw.dw_mysql_repository import DWMySQLRepository
+
+from app.repositories.mysql.meta.meta_mysql_repository import MetaMySQLRepository
 
 
 class MetaKnowledgeService:
-    def __init__(self):
-        pass
+    def __init__(self, meta_mysql_repository: MetaMySQLRepository, dw_mysql_repository: DWMySQLRepository):
+        self.meta_mysql_repository: MetaMySQLRepository = meta_mysql_repository
+        self.dw_mysql_repository: DWMySQLRepository = dw_mysql_repository
 
     async def build(self, config_path: Path):
         # 1.读取配置文件
@@ -16,9 +22,51 @@ class MetaKnowledgeService:
         meta_config: MetaConfig = OmegaConf.to_object(OmegaConf.merge(schema, context))
         print(meta_config.metrics)
 
-        # 2.根据配置文件同步指定的表信息和指标信息
+        # 2.根据配置文件同步指定的表信息
         if meta_config.tables:
-            pass
+            table_infos: list[TableInfoMySQL] = []
+            column_infos: list[ColumnInfoMySQL] = []
+            # 2.1 将表信息和字段信息保存meta数据库中
+            for table in meta_config.tables:
+                # table -> table_info
+                table_info = TableInfoMySQL(
+                    id=table.name,
+                    name=table.name,
+                    description=table.description
+                )
+                table_infos.append(table_info)
 
+                # 查询字段类型
+                column_types = await self.dw_mysql_repository.get_column_types(table.name)
+
+                for column in table.columns:
+                    # 查询字段取值示例
+                    column_values = await self.dw_mysql_repository.get_column_values(table.name, column.name)
+
+                    # column -> column_info
+                    column_info = ColumnInfoMySQL(
+                        id=f"{table.name}.{column.name}",
+                        name=column.name,
+                        type=column_types[column.name],
+                        role=column.role,
+                        examples=column_values,
+                        description=column.description,
+                        alias=column.alias,
+                        table_id=table.name
+                    )
+                    column_infos.append(column_info)
+
+            async with self.meta_mysql_repository.session.begin():
+                self.meta_mysql_repository.save_table_infos(table_infos)
+                self.meta_mysql_repository.save_column_infos(column_infos)
+
+            # 2.2 对字段信息建立向量索引
+
+            # 2.3 对指定的维度字段建立全文索引
+
+        # 3.根据配置文件同步指定的指标信息
         if meta_config.metrics:
             pass
+            # 3.1 将指标信息保存到meta数据库中0
+
+            # 3.2 对指标信息建立向量索引
