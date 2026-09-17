c65d75d72e8658db5d62458926251c11452569af
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2026-09-17 12:23:38 +0800

    根据配置信息保存表信息和字段信息到数据库

--- clients/mysql_client_manager.py
+++ clients/mysql_client_manager.py
@@ -29,7 +29,6 @@ dw_mysql_client_manager = MySQLClientManager(app_config.db_dw)
 
 if __name__ == '__main__':
     dw_mysql_client_manager.init()
-    engine = dw_mysql_client_manager.engine
 
 
     async def test():
